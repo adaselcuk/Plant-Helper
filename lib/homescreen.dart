449954edf79130f9934500a458c60dfa421a8ef5
Ada <adaselcuk16@gmail.com>
2024-05-23 21:04:42 +0300

    changed calendar to weekly and created function to automatically add events based on information user added

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -10,9 +10,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  CalendarFormat _calendarFormat = CalendarFormat.month;
+  CalendarFormat _calendarFormat = CalendarFormat.week;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  Map<DateTime, List> _events = {};
+
+  void generateEvents(int wateringFrequency, int fertilizingFrequency, int soilChangeFrequency) {
+    DateTime now = DateTime.now();
+    DateTime oneMonthFromNow = DateTime(now.year, now.month + 1, now.day);
+
+    for (int i = 0; i <= oneMonthFromNow.difference(now).inDays; i++) {
+      DateTime day = DateTime(now.year, now.month, now.day + i);
+
+      List<String> events = [];
+
+      if (i % wateringFrequency == 0) {
+        events.add('watering');
+      }
+      if (i % fertilizingFrequency == 0) {
+        events.add('fertilizing');
+      }
+      if (i % soilChangeFrequency == 0) {
+        events.add('soil change');
+      }
+
+      if (events.isNotEmpty) {
+        _events[day] = events;
+      }
+    }
+
+    setState(() {
+      _events = _events;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +64,32 @@ class _HomeScreenState extends State<HomeScreen> {
             _focusedDay = focusedDay;
           });
         },
+        eventLoader: (day) {
+          return _events[day] ?? [];
+        },
+        calendarBuilders: CalendarBuilders(
+          markerBuilder: (context, date, events) {
+            if (events.isNotEmpty) {
+              return Positioned(
+                bottom: 1,
+                child: Row(
+                  children: events.map((event) {
+                    if (event == 'watering') {
+                      return Icon(Icons.opacity, size: 10.0);
+                    } else if (event == 'fertilizing') {
+                      return Icon(Icons.eco, size: 10.0);
+                    } else if (event == 'soil change') {
+                      return Icon(Icons.grass, size: 10.0);
+                    } else{
+                      return Container();
+                    }
+                  }).toList(),
+                ),
+              );
+            }
+            return Container();
+          }
+        ),
         onFormatChanged: (format) {
           setState(() {
             _calendarFormat = format;
@@ -43,19 +99,21 @@ class _HomeScreenState extends State<HomeScreen> {
           _focusedDay = focusedDay;
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddPlantView(addPlant: (plant) {
-              Provider.of<PlantList>(context, listen: false).addPlant(plant);
-              Navigator.pop(context);
-            }
-            )),
-            
-          );
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(bottom: 16.0),
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddPlantView(addPlant: (plant) {
+                Provider.of<PlantList>(context, listen: false).addPlant(plant);
+                Navigator.pop(context);
+              }
+              )),
+            );
         },
         child: Icon(Icons.add),
+      ),
       ),
     );
   }
